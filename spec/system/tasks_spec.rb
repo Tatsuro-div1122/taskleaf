951930b53dc175ed1640bae37acd3566c3fba701
Tatsuro <tatsurow1122@gmail.com>
2020-08-09 23:03:12 +0900

    [Add] RSpecタスク新規投稿・編集

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -12,14 +12,15 @@ describe 'タスク管理機能', type: :system do
       click_button 'ログインする'
     end
 
+    shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+      it { expect(page).to have_content '最初のタスク' }
+    end
+
   describe '一覧表示機能' do
     context 'ユーザーAがログインしているとき' do
       let(:login_user) { user_a }
 
       it_behaves_like 'ユーザーAが作成したタスクが表示される'
-      shared_examples_for 'ユーザーAが作成したタスクが表示される' do
-        it { expect(page).to have_content '最初のタスク' }
-      end
     end
 
     context 'ユーザーBがログインしているとき' do
@@ -39,6 +40,56 @@ describe 'タスク管理機能', type: :system do
         visit task_path(task_a)
       end
 
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
+    end
+  end
+
+  describe '新規作成機能' do
+    let(:login_user) { user_a }
+
+    before do
+      visit new_task_path
+      fill_in '名称', with: task_name
+      click_button '登録する'
+    end
+
+    context '新規作成画面で名称を入力したとき' do
+      let(:task_name) { '新規作成のテストを書く' }
+
+      it '正常に登録される' do
+        expect(page).to have_selector '.alert-success', text: '新規作成のテストを書く'
+      end
+    end
+
+    context '新規作成画面で名称を入力しなかったとき' do
+      let(:task_name) { '' }
+
+      it 'エラーとなる' do
+        within '#error_explanation' do
+          expect(page).to have_content '名称を入力してください'
+        end
+      end
+    end
+  end
+
+  describe '編集機能' do
+    let( :login_user ) { user_a }
+
+    before do
+      visit new_task_path
+      fill_in 'name', with: '新規投稿'
+      click_button '登録する'
+      visit edit_task_path(task_a)
+      fill_in 'name', with: 'task_name'
+      click_button '更新する'
+    end
+
+    context '編集画面で名称を入力したとき' do
+      let( :task_name ) { '更新投稿' }
+
+      it '正常に更新される' do
+        expect(page).to have_selector '.alert-success', text: '更新投稿'
+      end
     end
   end
 end
