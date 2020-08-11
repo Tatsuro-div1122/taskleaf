fb2f3ee45f26de59c716cb1d4d4906b9d2819f51
Tatsuro <tatsurow1122@gmail.com>
2020-08-11 23:05:47 +0900

    タイムゾーン設定

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -34,11 +34,49 @@ describe 'ユーザー管理機能', type: :system do
 
       before do
         FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+        FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
         visit admin_users_path
       end
 
       it 'ユーザー一覧が表示される' do
         expect(page).to have_content 'ユーザーA'
+        expect(page).to have_content 'ユーザーB'
+      end
+    end
+  end
+
+  describe '詳細表示機能' do
+    context 'ユーザーAをクリックしたとき' do
+      let(:login_user) { admin }
+
+      before do
+        visit admin_user_path(user_a)
+      end
+
+      it 'ユーザーAの情報が表示される' do
+        expect(page).to have_content 'ユーザーA'
+      end
+
+      it '削除ボタンが表示される' do
+        expect(page).to have_content '削除'
+      end
+
+      it '表示ボタンが表示される' do
+        expect(page).to have_content '編集'
+      end
+    end
+  end
+
+  describe '新規ユーザー登録' do
+    context '新規登録ボタンを押下したとき' do
+      let(:login_user) { admin }
+
+      before do
+        visit new_admin_user_path
+      end
+
+      it '新規ユーザー作成画面が表示される' do
+        expect(page).to have_content 'ユーザー登録'
       end
     end
   end
