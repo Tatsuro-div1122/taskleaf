51f0e57f9de70ff105a94360cfcdf8a8a5e5461a
Tatsuro <tatsurow1122@gmail.com>
2020-08-13 23:57:22 +0900

    [Add] 確認画面

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :users
   end
 
-  resources :tasks
   root to: 'tasks#index'
+  resources :tasks do
+    post :confirm, action :confirm_new, on: :new
 end
